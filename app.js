bf8ac3c7b05db07f13ce9f1b9de3ed7e5995ac7a
Chris Jannenga <cjannenga312@gmail.com>
2019-12-30 14:28:46 -0700

    Created the auth middleware to handle authenticating requests

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://ranch-backend.herokuapp.com");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
